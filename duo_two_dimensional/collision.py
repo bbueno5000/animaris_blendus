32f8af55b3b994348c4d5f89f91326263c2caff1
bbueno5000 <bbueno5000@gmail.com>
2017-09-19 09:14:07 -0600

    added duo 2d scripts

---
+++ duo_two_dimensional/collision.py
@@ -0,0 +1,20 @@
+import bge
+
+scene = bge.logic.getCurrentScene()
+plane = scene.objects["Plane"]    
+
+cont = bge.logic.getCurrentController()
+own = cont.owner
+
+if own.sensors["Ray"].positive:
+    if own.sensors["Collision"].hitObject != plane:  
+        bge.render.drawLine(own.worldPosition, own.sensors["Ray"].hitPosition, (255, 0, 0))   
+        print("detection")  
+               
+if own.sensors["Ray"].positive and own.sensors["Collision"].positive:
+    if own.sensors["Collision"].hitObject != plane:   
+        print(own.sensors["Collision"].hitObject.name)
+        own.sensors["Collision"].hitObject.endObject()        
+        plane["survivors"] -= 1  
+        print("collision")
+
---
+++ duo_two_dimensional/time_limit.py
@@ -0,0 +1,1 @@
+print("time limit reached")
